d69ed052d00cf9d826e6b404f9fa8483ea0720df
Vinícius Mussak <vinicius.mussak@outlook.com>
2015-06-21 23:14:57 -0300

    First Commit

---
+++ src/FacebookAuthentication/Startup.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNet.Authentication.Facebook;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Diagnostics;
+using Microsoft.AspNet.Diagnostics.Entity;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
+using FacebookAuthentication.Models;
+
+namespace FacebookAuthentication
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            // Setup configuration sources.
+            var configuration = new Configuration()
+                .AddJsonFile("config.json")
+                .AddJsonFile($"config.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsEnvironment("Development"))
+            {
+                // This reads the configuration keys from the secret store.
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                configuration.AddUserSecrets();
+            }
+            configuration.AddEnvironmentVariables();
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<AppSettings>(Configuration.GetSubKey("AppSettings"));
+            
+            services.AddEntityFramework()
+                .AddSqlServer()
+                .AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
+            
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<FacebookAuthenticationOptions>(options =>
+            {
+                options.AppId = Configuration["Authentication:Facebook:AppId"];
+                options.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+            });
+          
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerfactory)
+        {
+            loggerfactory.AddConsole(minLevel: LogLevel.Warning);
+
+            if (env.IsEnvironment("Development"))
+            {
+                app.UseBrowserLink();
+                app.UseErrorPage(ErrorPageOptions.ShowAll);
+                app.UseDatabaseErrorPage(DatabaseErrorPageOptions.ShowAll);
+            }
+            else
+                app.UseErrorHandler("/Home/Error");
+
+            app.UseStaticFiles();
+            app.UseIdentity();
+            app.UseFacebookAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
